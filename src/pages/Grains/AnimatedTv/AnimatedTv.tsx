ffca1b94f729a3bb5bcd27c3318bf2f8105bfce6
Joyrudra Biswas <joyrudra@itobuz.com>
2025-01-07 12:49:03 +0530

    feat: added a reflection

--- src/pages/Grains/AnimatedTv/AnimatedTv.tsx
+++ src/pages/Grains/AnimatedTv/AnimatedTv.tsx
@@ -2,16 +2,29 @@ import "./AnimatedTv.scss";
 
 export default function AnimatedTv() {
   return (
-    <div className="animated-tv-container relative">
-      <div className="grainy" />
-      <div className="absolute z-10 top-0 left-0 w-full h-full flex justify-center items-center text-6xl font-bold text-white">
-        <p className="text-center text-red-600">
-          <span className="text-6xl">LOADS</span>
-          <span className="text-4xl ml-2">
-            of <br />
-          </span>
-          <em className="text-8xl underline">GRAINS</em>
-        </p>
+    <div>
+      <div className="animated-tv-container relative w-fit mx-auto">
+        <div className="absolute z-20 top-0 left-0 w-full h-full">
+          <div className="relative tv">
+            <img src="/backgrounds/tv.png" alt="tv" className="w-full h-full" />
+          </div>
+
+          {/* Reflection */}
+          <div className="absolute z-10 top-[100%] left-0 w-full h-full">
+            <div className="relative w-full h-full transform perspective-[1000px] rotate-x-[180deg]">
+              <img
+                src="/backgrounds/tv.png"
+                alt="tv-reflection"
+                className="w-full h-full opacity-30 blur-[5px] animate-wave"
+              />
+              <div className="absolute inset-0 bg-gradient-to-b from-transparent to-white/20" />
+            </div>
+          </div>
+        </div>
+
+        <div className="relative z-10 w-[80%] h-[80%] mx-auto my-[10%] overflow-hidden">
+          <div className="grainy absolute inset-0" />
+        </div>
       </div>
     </div>
   );
