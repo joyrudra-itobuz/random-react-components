0b310e7524f226e591e1144aba790c8dfcb2c7b1
Joyrudra Biswas <joyrudra@itobuz.com>
2025-08-27 17:19:43 +0530

    chore: added type as the default type

--- src/pages/MacLike/MacLike.tsx
+++ src/pages/MacLike/MacLike.tsx
@@ -6,12 +6,12 @@ import MacTitleBar from "../../ui/MacLike/TitleBar";
 export default function DesktopDemo() {
   return (
     <div className="relative h-screen w-screen bg-neutral-100">
-      {/* <MacWindow initialRect={{ x: 60, y: 60, w: 680, h: 420 }}>
+      <MacWindow initialRect={{ x: 60, y: 60, w: 680, h: 420 }}>
         <MacTitleBar title="Notes.txt" />
         <div className="p-4 h-[calc(100%-2.5rem)] overflow-auto">
           Window A content…
         </div>
-      </MacWindow> */}
+      </MacWindow>
 
       <MacWindow initialRect={{ x: 220, y: 140, w: 760, h: 520 }} inset={12}>
         <MacTitleBar title="Inspector" />
